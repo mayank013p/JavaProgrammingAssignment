f1a688332aba4c3a38d60a65607779406b46d2ff
Mayank Aitan <137062191+mayank013p@users.noreply.github.com>
2024-09-10 15:16:07 +0530

    Final

---
+++ src/Task/MessageConstants.java
@@ -0,0 +1,21 @@
+/***
+ * The program should run automatically once executed, and the user should
+ * have the option to switch between tasks and execute them as needed.
+ * Owner: Mayank Aitan
+ * Date of creation: 9 Sep 2024
+ */
+package Task;
+
+public class MessageConstants {
+    public final String mainMenuHeader = "Main Menu:";
+    public final String optionGenerateCombinations = "1. Generate combinations";
+    public final String optionDigitSumLoop = "2. Digit sum loop";
+    public final String optionFindConsecutiveSum = "3. Find Consecutive Sum";
+    public final String optionEncryptVariableShift = "4. Encryption With Variable Shift";
+    public final String optionEncodeHighestChars = "5. Encode The Digits";
+    public final String optionExit = "6. Exit";
+    public final String promptEnterOption = "Enter option number: ";
+    
+    public final String invalidOption = "Invalid option! Please choose a valid option.";
+    public final String exitMessage = "Exiting the program.";
+}
